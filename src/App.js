e8920b81018da45bba5bd1fbf8c947cb5698d55f
Yves Loeys <yves.loeys@gmail.com>
2021-01-15 13:30:44 -0800

    creating button component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import './App.css';
+import React, { Component } from 'react';
+import Button from "./components/Button"
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Cleanup</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state ={
+      current: '0',
+      previous:[]
+    }
+  }
+
+  reset=()=>{
+    this.setState({result:'0'})
+  }
+
+  addToCurrent=(symbol)=>{
+    this.setState({current:this.state.current + symbol})
+  }
+  render(){
+    const buttons = [
+      {symbol:'C',cols:1,action:this.reset},
+      {symbol:'/',cols:1,action:this.addToCurrent},
+      {symbol:'7',cols:1,action:this.addToCurrent},
+      {symbol:'8',cols:1,action:this.addToCurrent},
+      {symbol:'9',cols:1,action:this.addToCurrent},
+      {symbol:'X',cols:1,action:this.addToCurrent},
+      {symbol:'4',cols:1,action:this.addToCurrent},
+      {symbol:'5',cols:1,action:this.addToCurrent},
+      {symbol:'6',cols:1,action:this.addToCurrent},
+      {symbol:'-',cols:1,action:this.addToCurrent},
+      {symbol:'1',cols:1,action:this.addToCurrent},
+      {symbol:'2',cols:1,action:this.addToCurrent},
+      {symbol:'3',cols:1,action:this.addToCurrent},
+      {symbol:'+',cols:1,action:this.addToCurrent},
+      {symbol:'0',cols:1,action:this.addToCurrent},
+      {symbol:'.',cols:1,action:this.addToCurrent},
+      {symbol:'=',cols:1,action:this.addToCurrent},
+    ]
+    return (
+      <div className="App">
+        <input className="result" type="text" value={this.state}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App

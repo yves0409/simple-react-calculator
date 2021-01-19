3ab7785ca509dc3dad53ecffcd94103881da08d9
Yves Loeys <yves.loeys@gmail.com>
2021-01-19 09:51:37 -0800

    error fix floaty

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ class App extends Component {
     }
   }
 
-  reset=()=>{
+  reset = () => {
     this.setState({current:'0',previous:[],nextIsReset:false})
   }
 
   addToCurrent=(symbol)=>{
     console.log('symbol');
-    if(["/","-","+","X"].indexOf(symbol) > -1){
+    if(["/","-","+","*"].indexOf(symbol) > -1){
       let {previous} = this.state
       previous.push(this.state.current +symbol);
       this.setState({previous,nextIsReset:true})
@@ -38,7 +38,7 @@ class App extends Component {
       let {current,previous,nextIsReset}= this.state
       if(previous.length > 0){
         current = eval(String(previous[previous.length -1] + current));
-        this.setState({current,previous,nextIsReset:true})
+        this.setState({current,previous: [],nextIsReset:true})
       }
     }
 
@@ -50,7 +50,7 @@ class App extends Component {
       {symbol:'7',cols:1,action:this.addToCurrent},
       {symbol:'8',cols:1,action:this.addToCurrent},
       {symbol:'9',cols:1,action:this.addToCurrent},
-      {symbol:'X',cols:1,action:this.addToCurrent},
+      {symbol:'*',cols:1,action:this.addToCurrent},
       {symbol:'4',cols:1,action:this.addToCurrent},
       {symbol:'5',cols:1,action:this.addToCurrent},
       {symbol:'6',cols:1,action:this.addToCurrent},
@@ -66,7 +66,7 @@ class App extends Component {
     return (
       
       <div className="App">
-      {this.state.previous.lenght > 0 ? 
+      {this.state.previous.length > 0 ? 
         <div className="floaty-last">{this.state.previous[this.state.previous.length -1]}</div> : null}
         <input className="result" type="text" value={this.state.current}/>
         {buttons.map((btn,i)=> {
